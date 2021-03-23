81a9bc878f8825bd66a4cbba26db9ce01397c455
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-03-23 23:16:19 +0530

    Move everything to OpenLand directory.

--- package.bat
+++ package.bat
@@ -8,14 +8,19 @@ xcopy OpenLandGrass\Config ToPackage\OpenLandGrass\Config /E/H
 del ToPackage\OpenLandGrass\Config\DefaultInput.ini
 xcopy OpenLandGrass\OpenLandGrass.uproject ToPackage\OpenLandGrass
 
-md ToPackage\OpenLandGrass\Content
-md ToPackage\OpenLandGrass\Content\OpenLandGrass
-xcopy OpenLandGrass\Content\OpenLandGrass ToPackage\OpenLandGrass\Content\OpenLandGrass /E/H
-
-md ToPackage\OpenLandGrass\Content\TinyLand
-xcopy OpenLandGrass\Content\TinyLand ToPackage\OpenLandGrass\Content\TinyLand /E/H
-
 md ToPackage\OpenLandGrass\Content\OpenLand
 md ToPackage\OpenLandGrass\Content\OpenLand\Core
 md ToPackage\OpenLandGrass\Content\OpenLand\Core\RVT
 xcopy OpenLandGrass\Content\OpenLand\Core\RVT ToPackage\OpenLandGrass\Content\OpenLand\Core\RVT /E/H
+
+md ToPackage\OpenLandGrass\Content\OpenLand\TinyLand
+xcopy OpenLandGrass\Content\OpenLand\TinyLand ToPackage\OpenLandGrass\Content\OpenLand\TinyLand /E/H
+
+md ToPackage\OpenLandGrass\Content\OpenLand\OpenLandGrass
+xcopy OpenLandGrass\Content\OpenLand\OpenLandGrass ToPackage\OpenLandGrass\Content\OpenLand\OpenLandGrass /E/H
+
+md ToPackage\OpenLandGrass\Content\OpenLand\Maps
+xcopy OpenLandGrass\Content\OpenLand\Maps\ExampleOpenLandGrassMap.umap ToPackage\OpenLandGrass\Content\OpenLand\Maps
+
+md ToPackage\OpenLandGrass\Content\OpenLand\Maps\Utils
+xcopy OpenLandGrass\Content\OpenLand\Maps\Utils ToPackage\OpenLandGrass\Content\OpenLand\Maps\Utils /E/H
