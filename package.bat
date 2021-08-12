7298876a12a1338a43a67f431fe8fb106c286158
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2021-08-12 08:05:56 +0530

    Get changes from OpenLand RVT Tools.

--- package.bat
+++ package.bat
@@ -13,6 +13,15 @@ md ToPackage\OpenLandGrass\Content\OpenLand\Core
 md ToPackage\OpenLandGrass\Content\OpenLand\Core\RVT
 xcopy OpenLandGrass\Content\OpenLand\Core\RVT ToPackage\OpenLandGrass\Content\OpenLand\Core\RVT /E/H
 
+md ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials
+md ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Integrations
+copy OpenLandGrass\Content\OpenLand\Core\Materials\Integrations\OpenLand_Camera_Depth.uasset ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Integrations\OpenLand_Camera_Depth.uasset
+copy OpenLandGrass\Content\OpenLand\Core\Materials\Integrations\Use_OpenLand_RVT_Tools.uasset ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Integrations\Use_OpenLand_RVT_Tools.uasset
+
+md ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Functions
+md ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Functions\Utils
+xcopy OpenLandGrass\Content\OpenLand\Core\Materials\Functions\Utils ToPackage\OpenLandGrass\Content\OpenLand\Core\Materials\Functions\Utils /E/H
+
 md ToPackage\OpenLandGrass\Content\OpenLand\TinyLand
 xcopy OpenLandGrass\Content\OpenLand\TinyLand ToPackage\OpenLandGrass\Content\OpenLand\TinyLand /E/H
 
@@ -21,6 +30,9 @@ xcopy OpenLandGrass\Content\OpenLand\OpenLandGrass ToPackage\OpenLandGrass\Conte
 
 md ToPackage\OpenLandGrass\Content\OpenLand\Maps
 xcopy OpenLandGrass\Content\OpenLand\Maps\ExampleOpenLandGrassMap.umap ToPackage\OpenLandGrass\Content\OpenLand\Maps
+
+md ToPackage\OpenLandGrass\Content\OpenLand\Widgets
+copy OpenLandGrass\Content\OpenLand\Widgets\OpenLand_RVT_Widget.uasset ToPackage\OpenLandGrass\Content\OpenLand\Widgets\OpenLand_RVT_Widget.uasset
 
 md ToPackage\OpenLandGrass\Content\OpenLand\Maps\Utils
 xcopy OpenLandGrass\Content\OpenLand\Maps\Utils ToPackage\OpenLandGrass\Content\OpenLand\Maps\Utils /E/H
